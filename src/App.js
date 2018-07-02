05f133c2b5cf15bbff304358c1d72a3e71c0eb64
Rahul Pargi <rahulpargi@gmail.com>
2018-07-01 21:29:17 -0500

    Adding Maps

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-
+import GoogleMapReact from 'google-map-react';
 import './App.css';
 import Flat from './components/flat';
+import Marker from './components/marker'
 
 class App extends Component {
   constructor(props){
@@ -10,8 +11,28 @@ class App extends Component {
       flats:[]
     };
   }
-  
+
+  componentDidMount(){
+    const url="https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url)
+    .then(response=>response.json())
+    .then((data)=>{
+      console.log(data);
+      this.setState({
+        flats:data
+      });
+    })
+  }
+;
   render() {
+    
+  const center={
+   
+    lat: 48.8566,
+    lng: 2.3522
+  
+}
+const key="AIzaSyAIW-eWe8hZlWjIc5o3V7wcmI3Z0GTpbS4";
      return (
       <div className="app">
           <div className="main">
@@ -25,6 +46,15 @@ class App extends Component {
             
           </div>
           <div className="map">
+          <GoogleMapReact
+          bootstrapURLKeys={{ key:key}}
+          center={center}
+          zoom={11}
+        >
+         {this.state.flats.map((flat)=>{
+                return <Marker lat={flat.lat} lng={flat.lng} text={flat.price}/>
+              })}
+        </GoogleMapReact>
           </div>
       </div>
     );
